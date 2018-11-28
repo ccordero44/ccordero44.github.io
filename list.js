136a3f183de60b4652548d1df88dc0657c008fd2
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2018-11-27 23:21:52 -0600

    Update list.js

--- list.js
+++ list.js
@@ -3,7 +3,6 @@ var shops = [
    	["Procision Auto Body", "2701 SW Adams St", "Peoria, IL 61602", " 309-637-1946", " 309-637-1397", " procision@sbcglobal.net", 40.672276, -89.61940799999999, " Email Tow Request to shop"],
 	["United Collision & Customs", "3003 S State St", "Lockport, IL 60441", " 815-774-3223", " 779-205-3381", " UnitedCollision3003@yahoo.com", 41.563804, -88.06929919999999, " Email Tow Request to shop (repairable only)"],
 	["Durans Body Shop", "2610 W Chicago Ave", "Chicago, IL 60622", " 773-486-3333", " 773-486-3247", " duransbodyshop@aol.com", 41.8960018, -87.6923845, " Email Tow Request to Wes's"],
-	["Orlando Glass", "641 N Ashland Ave", "Chicago, IL 60622", " 312-243-0776", " 312-243-8234", " orlando.auto@att.net", 41.89361299999999, -87.666804, " Glass & Upholstery"],
 	["Classic Collision Center", "8583 S South Chicago Ave", "Chicago, IL 60617", " 773-731-9191", " 708-828-5319", " cautorebui@aol.com", 41.7390034, -87.5678942, " Email Tow Request to Wes's"],
 	["Tony's Collision Care Center", "8219 Christiana Ave", "Skokie, IL 60076", " 847-673-9630", " 847-673-9631", " tonyscollision@aol.com", 42.0306338, -87.7121477, " Email Tow Request to Wes's"],
 	["Autodome", "2729 Belvidere Rd", "Waukegan, IL 60085", " 847-406-3040", " 847-406-3045", " autodome@gmail.com", 42.3491794, -87.8696149, " Email Tow Request to shop (repairable only)"],
