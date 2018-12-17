f8fc8c7923a2f7dd7f059693b8631e3cf3e94405
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2018-12-17 14:41:26 -0600

    Update appraisers.js

--- appraisers.js
+++ appraisers.js
@@ -2,7 +2,12 @@ var Bob = {
 center: {lat: 42.0346082, lng: -87.9426643},
 radius: 35
 };
+var Jose = {
+center: {lat: 41.878114, lng: -87.629798},
+  radius: 15
+};
 
 var appraisers = [
-  {name: "Bob", coord: Bob.center, circle: Bob.radius, desc: "Bob's inspection area"}
+  {name: "Bob", coord: Bob.center, circle: Bob.radius, desc: "Bob's inspection area"},
+  {name: "Jose", coord: Jose.center, circle: Jose.radius, desc: "Jose's inspection area"}
   ];
