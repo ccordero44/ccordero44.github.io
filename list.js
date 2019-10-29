a8b0e89590b82e14edfce10217a58fca19ac5de4
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2019-10-29 15:56:18 -0500

    Add files via upload

--- list.js
+++ list.js
@@ -7,7 +7,7 @@ var shops = [
 	["Tony's Collision Care Center", "8219 Christiana Ave", "Skokie, IL 60076", " 847-673-9630", " 847-673-9631", " tonyscollision@aol.com", 42.0306338, -87.7121477, " Email Tow Request to Wes's"],
 	["Autodome", "2729 Belvidere Rd", "Waukegan, IL 60085", " 847-406-3040", " 847-406-3045", " autodome@gmail.com", 42.3491794, -87.8696149, " Email Tow Request to shop"],
 	["Auto Lodge", "2617 IL-47", "Woodstock, IL 60098", " 847-669-0339", " 630-871-1462", " autocrafters47@gmail.com", 42.2898579, -88.43376259999999, " Call A to Z Towing"],
-	["Super Sports Automotive", "1530 Hubbard Ave, Unit #E2", "Batavia, IL 60510", " 630-326-9443", " 855-564-2101", " info@supersportsautomotive.com", 41.8594884, -88.27520299999999, " Email Tow Request to shop"],
+	["Super Sports Automotive", "1530 Hubbard Ave, Unit #E2", "Batavia, IL 60510", " 630-326-9443", " 855-564-2101", " info@supersportsautomotive.com", 41.859036, -88.275204, " Email Tow Request to shop"],
 	["Goshen Collision Center", "1227 E Lincoln Ave", "Goshen, IN 46528", " 574-534-8812", " 574-534-4585", " goshencollision@aol.com", 41.589936, -85.816257, " Email Tow Request to shop"],
 	["Miller Collision Center", "5200 Melton Rd.", "Gary, IN 46403", " 219-938-9999", " 219-938-4660", " millercollision@gmail.com", 41.596677, -87.275278, " Email Tow Request to shop"],
 	["Williamson Collision Center", "4965 Massachusetts Ave", "Indianapolis, IN 46218", " 317-545-6471", " 317-545-6750", " carzz@indy.rr.com", 39.808958, -86.086781, " Email Tow Request to shop"]
