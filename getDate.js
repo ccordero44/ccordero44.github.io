4eb6a02caa22309c6b53f63a031eee6ed2aa637d
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2020-03-13 23:21:18 -0500

    Update getDate.js

--- getDate.js
+++ getDate.js
@@ -3,6 +3,6 @@ var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
-
-return mm + '/' + dd + '/' + yyyy;
+var myDate = mm + '/' + dd + '/' + yyyy;
+return myDate;
 }
