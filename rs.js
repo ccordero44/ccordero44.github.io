a49a67688687dcc6169f1a64cb7f40ec83ff9e46
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2020-07-15 14:37:43 -0500

    Update rs.js

--- rs.js
+++ rs.js
@@ -102,6 +102,7 @@ $('#save').click(function () {
 	//$('.selectThis').keypress(function(e){ return e.which != 13; });
 	$("input:radio").change(function () {
 	setCookie($(this).attr('name'), $(this).val());	
+	console.log($(this).attr('name') + " " + $(this).val())
 		if ($("input[name^=TERRITORY]").prop("checked") == true) {
 			$('#checkbox9').prop("checked",true);
 			$('#checkbox9').attr("disabled",true);
