5afe818b2a084f99073e87a71d38a03ebc577619
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2022-07-20 11:16:13 -0500

    Update shops.js

--- Shop Locator/shops.js
+++ Shop Locator/shops.js
@@ -7,7 +7,7 @@
 		  
 $.ajax({
    type: "GET",
-   url: "file:\\10.74.11.100\unique_data\Claims\TOOLKIT\Shop%20Locator\shops.csv",
+   url: "file:\\\\10.74.11.100\\unique_data\\Claims\\TOOLKIT\\Shop%20Locator\\shops.csv",
    async: false,
    success: function(CSVdata) { 
 		var data = $.csv.toArrays(CSVdata);
