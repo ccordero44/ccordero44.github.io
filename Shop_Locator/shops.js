837ed51bc7a3154b1f7dcff76835a8c1b451a237
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2022-07-20 12:35:32 -0500

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -7,7 +7,7 @@
 		  
 $.ajax({
    type: "GET",
-   url: "https://producersnational-my.sharepoint.com/:x:/p/ccordero/Eba4ny_YdlZBm6QwTXjCCfEBRt1PwMgPAC7fN_ijZQApmg?e=X88eeP",
+   url: "shops.csv",
    async: false,
    success: function(CSVdata) { 
 		var data = $.csv.toArrays(CSVdata);
