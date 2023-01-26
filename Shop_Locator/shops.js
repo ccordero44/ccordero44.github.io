38ccd404ba613a3da602530a0042e75cfdd86c28
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2023-01-25 18:36:17 -0600

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -5,11 +5,21 @@
 			// shops = data;
 		  // });
 		  
-$.ajax({
+/*$.ajax({
    type: "GET",
    url: "shops.csv",
    async: false,
    success: function(CSVdata) { 
 		var data = $.csv.toArrays(CSVdata);
 		shops = data; }
+});*/
+
+$.ajax({
+   type: "GET",
+   url: "",
+   async: false,
+    headers: {  'Access-Control-Allow-Origin': 'https://producersnational.sharepoint.com/sites/ProducersNational/Shared%20Documents/General/shops.csv' },
+   success: function(CSVdata) { 
+		var data = $.csv.toArrays(CSVdata);
+		console.log(data); }
 });
