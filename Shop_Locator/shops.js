70bb50f859c19410c4100af60ef98ab3a2097503
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2023-01-26 14:08:25 -0600

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -21,35 +21,33 @@ $.ajax({
    url: "https://producersnational.sharepoint.com/_api/web/currentuser",
    async: false,
    success: function(CSVdata) { 
-	console.log('logged in');
-	console.log(CSVdata);
-	statusCode = 1;
-	   var data = $.csv.toArrays(CSVdata);
-	shops = data;
-$.ajax({
-   type: "GET",
-   url: "https://producersnational.sharepoint.com/sites/ProducersNational/Shared%20Documents/General/shops.csv",
-   async: false,
-   success: function(CSVdata) { 
+		console.log('logged in');
+		console.log(CSVdata);
+		statusCode = 1;
 		var data = $.csv.toArrays(CSVdata);
-	   shops = data;
-		console.log(data); }
-});
-}, 
-   error: function(CSVdata) {
-
-       
-	   var newwindow = window.open('https://producersnational.sharepoint.com');
-	console.log('logged out');
-	statusCode = 0;
-	
-	var answer = confirm('Please log into PNC Sharepoint then click OK.');
-	   if (answer) {
-		newwindow.close();
-	location.reload();
-	   }else{
-	location.reload();
-   };
-	
+		shops = data;
+		$.ajax({
+		   type: "GET",
+		   url: "https://producersnational.sharepoint.com/sites/ProducersNational/Shared%20Documents/General/shops.csv",
+		   async: false,
+		   success: function(CSVdata) { 
+				var data = $.csv.toArrays(CSVdata);
+			   shops = data;
+				console.log(data); }
+		});
+	}, 
+   error: function(data) {
+	    var newwindow = window.open('https://producersnational.sharepoint.com');
+		console.log('logged out');
+		statusCode = 0;
+		
+		var answer = confirm('Please log into PNC Sharepoint then click OK.');
+		   if (answer) {
+			newwindow.close();
+		location.reload();
+		   }else{
+		location.reload();
+	   };
+	return;
 }
 });
