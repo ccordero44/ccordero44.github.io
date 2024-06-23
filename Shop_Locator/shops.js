a1f42eac7e150d77412c29bcde36e51ab5d1c396
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-06-23 15:49:03 -0500

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -16,7 +16,7 @@ $.ajax({
 						var _coords = $(this).find('Point').find('coordinates').text().trim().split(',');
 						var _towingIcon = $(this).find('styleUrl').text();
 						var polyCoords = $(this).find('Polygon'); //.find('outerBoundaryIs').find('LinearRing').find('coordinates').text().trim();
-						console.log(polyCoords.html());
+						console.log(polyCoords.html().split('\n'));
 						if (polyCoords.length > 0) {
 							//for (let i = 0; i < polyCoords.length; i++) {
 							$(polyCoords).each(function () {
