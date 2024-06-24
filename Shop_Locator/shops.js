38e785d3310af3ccc89bd60d84b050b7b397db5b
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-06-24 00:50:39 -0500

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -14,7 +14,7 @@ $.ajax({
 				$(response).find('Style').each(function() {
 					var style = {
 						id: $(this).attr('id'),
-						color: $(this).find('PolyStyle').find('color').text().trim();
+						color: $(this).find('PolyStyle').find('color').text().trim()
 					};
 					styles.push(style);	
 							});
