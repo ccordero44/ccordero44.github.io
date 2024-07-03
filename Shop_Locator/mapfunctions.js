e46f1fd2eae24fd2e4e18ca93e2e7f089ed40f39
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-03 16:31:24 -0500

    Update mapfunctions.js

--- Shop_Locator/mapfunctions.js
+++ Shop_Locator/mapfunctions.js
@@ -1449,3 +1449,25 @@ function setHailMarkers() {
             }
   		
  }
+function setDatepicker(_this) {
+ 
+            /* Get the parent class name so we 
+                can show date picker */
+            let className = $(_this).parent()
+                .parent().parent().attr('class');
+ 
+            // Remove space and add '.'
+            let removeSpace = className.replace(' ', '.');
+ 
+            // jQuery class selector
+            $("." + removeSpace).datepicker({
+                format: "dd/mm/yyyy",
+ 
+                // Positioning where the calendar is placed
+                orientation: "bottom auto",
+                // Calendar closes when cursor is 
+                // clicked outside the calendar
+                autoclose: true,
+                showOnFocus: "false"
+            });
+        }
