0f9105ebb0acae38b5aa84f975cbc0a4a610a506
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-03 22:23:22 -0500

    Update mapfunctions.js

--- Shop_Locator/mapfunctions.js
+++ Shop_Locator/mapfunctions.js
@@ -1451,23 +1451,18 @@ function setHailMarkers() {
  }
 function setDatepicker(_this) {
  
-            /* Get the parent class name so we 
-                can show date picker */
-            let className = $(_this).parent()
-                .parent().parent().attr('class');
- 
-            // Remove space and add '.'
-            let removeSpace = className.replace(' ', '.');
- 
-            // jQuery class selector
-            $("." + removeSpace).datepicker({
-                format: "dd/mm/yyyy",
- 
-                // Positioning where the calendar is placed
-                orientation: "bottom auto",
-                // Calendar closes when cursor is 
-                // clicked outside the calendar
-                autoclose: true,
-                showOnFocus: "false"
-            });
+            $('#dialog').dialog({
+	            open: function() {
+	                $("#dialogDate").datepicker({
+		                format: "dd/mm/yyyy",
+		                orientation: "bottom auto",
+		                autoclose: true,
+		                showOnFocus: "false"
+		            }).blur();
+	            },
+	            close: function() {
+	                $('#dialogDate').datepicker('destroy');
+	            },
+	        });
+            
         }
