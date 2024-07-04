160fd35169fd7261dd3cfb08ec2e397db094348c
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-04 01:58:01 -0500

    Update hail.js

--- Shop_Locator/hail.js
+++ Shop_Locator/hail.js
@@ -46,7 +46,8 @@ $.ajax({
 				
 			}, 
    error: function(data) {
-
+	   console.log(data.responseJSON.error);
+	callHailMarkers(new Date().getFullYear()+String(new Date().getMonth() + 1).padStart(2, '0')+String(new Date().getDate()).padStart(2, '0'));
 	return;
 }
 });
