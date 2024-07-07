7205722d1236079447fa5e3f3a5a220b48228810
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-07 02:21:35 -0500

    Update mapfunctions.js

--- Shop_Locator/mapfunctions.js
+++ Shop_Locator/mapfunctions.js
@@ -1466,7 +1466,7 @@ function setDatepicker(_this) {
 		            }).blur(function() {
 				if (this.value === '') this.value = String(new Date().getMonth() + 1).padStart(2, '0') + "/" + String(new Date().getDate()).padStart(2, '0') + "/" + new Date().getFullYear();
 			});
-			    //$("#dialogDate").datepicker({title:'Test Dialog'}).blur();
+			    $("#dialogDate").datepicker("show");
 	            },
 	            close: function() {
 	                $('#dialogDate').datepicker('destroy');
