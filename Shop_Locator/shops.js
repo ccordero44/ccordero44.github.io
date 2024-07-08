40dce8b0555bb1a1e5d837e67f08ef44834b30a5
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-08 17:02:39 -0500

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -268,7 +268,7 @@ $.ajax({
 						//console.log(_desc.toString().replace("<![CDATA[", "").replaceAll("<br>", ",").replace("]]>", ""));
 						if (_coords.length > 1) {
 						var _rateTablesArray = _desc.toString().split("<br>")
-						var _ratetables = rateTableArray.filter(element => _rateTablesArray .includes(element))[0]; //_desc.toString().split("<br>")[2];
+						var _ratetables = rateTableArray.filter(element => _rateTablesArray.includes(element.trim()))[0]; //_desc.toString().split("<br>")[2];
 						
 						//var _phone = _desc.toString().replace("<![CDATA[", "").replaceAll("<br>", ",").replace("]]>", "").split(",")[0].replace("Ph# ", "");
 						var _phones = _desc.toString().match(/((?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?)/img);
