eaef7c04a51220315f54889424e8960041bb8596
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-09 11:46:42 -0500

    Update mapfunctions.js

--- Shop_Locator/mapfunctions.js
+++ Shop_Locator/mapfunctions.js
@@ -202,7 +202,7 @@
   function clicky() {
 
 
-	if (checkEleCollision.collide($('#testDiv')[0], $('#sync')[0]))  $('#testDiv').css('visibility','hidden');
+	
        	//document.getElementById("testDiv").style.display = "none";
         document.getElementById("dvTable").style.display = "none";
             infowindow.close();
@@ -358,7 +358,7 @@
                 //});
 
 
-
+if (checkEleCollision.collide($('#testDiv')[0], $('#sync')[0]))  $('#testDiv').css('visibility','hidden');
         }
 		
 	/* function testtable() {
