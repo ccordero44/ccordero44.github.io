6140227d6d1b0651ff2976406a68c0b4736a764a
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-12 23:56:54 -0500

    Update redLightCams.js

--- Shop_Locator/redLightCams.js
+++ Shop_Locator/redLightCams.js
@@ -3,7 +3,7 @@ $.ajax({
     url: "https://data.cityofchicago.org/resource/spqx-js37.json",
     type: "GET",
     data: {
-      "$limit" : 5000,
+      "$limit" : 100000,
       "$$app_token" : "C0x9RtLoTQDig0aJJt24jGtfS"
     }
 }).done(function(data) {
