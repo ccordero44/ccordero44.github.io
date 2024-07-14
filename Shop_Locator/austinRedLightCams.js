0a5fefa84862d387202993b264d71499f5393f5b
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-13 19:43:30 -0500

    Update austinRedLightCams.js

--- Shop_Locator/austinRedLightCams.js
+++ Shop_Locator/austinRedLightCams.js
@@ -12,8 +12,9 @@ $.ajax({
 				var count = Number(redCamMarkers.length);
 				var lng,lat,id,camStatus,detDirection;
 				for (var i = 0; i < data.features.length; i++) {
-					count += 1;
-                        if (data.features[i].geometry !== null && (data.features[i].properties.detector_status === 'OK' || data.features[i].properties.detector_status === 'OK - MINOR ISSUE')) {
+					
+     if (data.features[i].geometry !== null && (data.features[i].properties.detector_status === 'OK' || data.features[i].properties.detector_status === 'OK - MINOR ISSUE')) {
+						count += 1;
 							var lng,lat,id,camStatus,detDirection;
 							lng = data.features[i].geometry.coordinates[0];
 							lat = data.features[i].geometry.coordinates[1];
