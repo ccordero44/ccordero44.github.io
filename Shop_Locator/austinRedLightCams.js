df3b9a9330cf6bd044ac639b7e1ca2daa8c72abb
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-07-15 19:32:12 -0500

    Update austinRedLightCams.js

--- Shop_Locator/austinRedLightCams.js
+++ Shop_Locator/austinRedLightCams.js
@@ -1,7 +1,7 @@
 var austinRedCamMarker, austinRedCamMarkers = [];
 $.ajax({
 			   type: "GET",
-			   url: "https://data.austintexas.gov/resource/qpuw-8eeb.geojson?$limit=10000",
+			   url: "https://data.austintexas.gov/resource/b4k4-adkb.geojson?$limit=10000",
 			   
 			}).done(function(data) {
 				var redCam = {
@@ -13,14 +13,14 @@ $.ajax({
 				var lng,lat,id,camStatus,detDirection;
 				for (var i = 0; i < data.features.length; i++) {
 					
-     if (data.features[i].geometry !== null && (data.features[i].properties.detector_status === 'OK' || data.features[i].properties.detector_status === 'OK - MINOR ISSUE')) {
+     if (data.features[i].geometry !== null && (data.features[i].properties.camera_status === 'TURNED_ON' )) {
 						count += 1;
 							var lng,lat,id,camStatus,detDirection;
 							lng = data.features[i].geometry.coordinates[0];
 							lat = data.features[i].geometry.coordinates[1];
-							id = data.features[i].properties.detector_id;
-							camStatus = data.features[i].properties.detector_status;
-							detDirection = data.features[i].properties.detector_direction;
+							id = data.features[i].properties.camera_id;
+							camStatus = data.features[i].properties.camera_status;
+							detDirection = data.features[i].properties.primary_st;
 							austinRedCamMarker = new google.maps.Marker({
 								position: new google.maps.LatLng(lat, lng),
 								clickable: true,
@@ -29,7 +29,7 @@ $.ajax({
 
 							});
 						austinRedCamMarkers.push(austinRedCamMarker);
-						addInfoWindow(austinRedCamMarker, '<b>Red Light Camera ID #' + id + '</b><br />Intersection: ' + data.features[i].properties.location_name + '<br />Direction: ' + detDirection + '<br /><a href="https://austin-portal.ecourt.com/public-portal/?q=node/412" target="_blank">Search for Tickets</a><br /><a href="https://APD-austintx.govqa.us/WEBAPP/_rs/RequestOpen.aspx?rqst=3" target="_blank">FOIA Request</a>');
+						addInfoWindow(austinRedCamMarker, '<b>Red Light Camera ID #' + id + '</b><br />Intersection: ' + data.features[i].properties.location_name + '<br />Primary Street: ' + detDirection + '<br /><a href="https://austin-portal.ecourt.com/public-portal/?q=node/412" target="_blank">Search for Tickets</a><br /><a href="https://APD-austintx.govqa.us/WEBAPP/_rs/RequestOpen.aspx?rqst=3" target="_blank">FOIA Request</a>');
       
 						}
 						
