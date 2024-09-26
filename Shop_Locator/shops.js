801b831a44ee4c77e8204495a7baf234002934bf
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-09-26 09:41:27 -0500

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -174,10 +174,10 @@ $.ajax({
 						
 					   // if (typeof storedShops[index] !== 'undefined' && (Number(_coords[1]) !== Number(storedShops[index][6]) || Number(_coords[0]) !== Number(storedShops[index][7]))) {
 					var newIndex = storedShops.findFirstSubstring(Number(_coords[1]));
-					if (newIndex === -1) newIndex = storedShops.findFirstSubstring(Number(_coords[0]));	
+					//if (newIndex === -1) newIndex = storedShops.findFirstSubstring(Number(_coords[0]));	
 					//if (newIndex === -1) newIndex = storedShops.findFirstSubstring(newShop[0]);
-					//if (typeof storedShops[newIndex] !== 'undefined' || newIndex === -1) {	
-					if ((typeof storedShops[newIndex] !== 'undefined' && Number(_coords[1]) !== Number(storedShops[newIndex][6])) || newIndex === -1) {
+					if (typeof storedShops[newIndex] !== 'undefined' || newIndex === -1) {	
+					//if ((typeof storedShops[newIndex] !== 'undefined' && Number(_coords[1]) !== Number(storedShops[newIndex][6])) || newIndex === -1) {
 						//&& (Number(_coords[1]) !== Number(storedShops[newIndex][6])) || Number(_coords[0]) !== Number(storedShops[newIndex][7])
 					if (newShop[0] !== 'UNIQUE/LIGHTHOUSE') {
 					 $.ajax({
