4417683b127fae78bb7612bb1c969d1fbb7608a1
ccordero44 <39055940+ccordero44@users.noreply.github.com>
2024-09-26 09:30:49 -0500

    Update shops.js

--- Shop_Locator/shops.js
+++ Shop_Locator/shops.js
@@ -176,8 +176,8 @@ $.ajax({
 					var newIndex = storedShops.findFirstSubstring(Number(_coords[1]));
 						
 					//if (newIndex === -1) newIndex = storedShops.findFirstSubstring(newShop[0]);
-					if (typeof storedShops[newIndex] !== 'undefined' && newIndex === -1) {	
-					//if ((typeof storedShops[newIndex] !== 'undefined' && Number(_coords[1]) !== Number(storedShops[newIndex][6])) || newIndex === -1) {
+					//if (typeof storedShops[newIndex] !== 'undefined' || newIndex === -1) {	
+					if ((typeof storedShops[newIndex] !== 'undefined' && Number(_coords[1]) !== Number(storedShops[newIndex][6])) || newIndex === -1) {
 						//&& (Number(_coords[1]) !== Number(storedShops[newIndex][6])) || Number(_coords[0]) !== Number(storedShops[newIndex][7])
 					if (newShop[0] !== 'UNIQUE/LIGHTHOUSE') {
 					 $.ajax({
